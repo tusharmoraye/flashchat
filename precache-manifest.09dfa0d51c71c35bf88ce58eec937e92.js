8305da7fbf76a133d2fd73cf7fbfd3903e09c3c5
tusharmoraye <tusharmoraye@gmail.com>
2020-09-07 07:37:22 +0000

    Deploying to gh-pages from  @ c19670fcc39c20688aa313c248ff7a2b58c0b673 🚀

---
+++ precache-manifest.09dfa0d51c71c35bf88ce58eec937e92.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "49b7463478d2a5f9f0d010a767db7c32",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2f79d3c5e65f54fdd2bc",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "b4268a936689dd9bb147",
+    "url": "/static/js/2.46897638.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.46897638.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2f79d3c5e65f54fdd2bc",
+    "url": "/static/js/main.d83acdba.chunk.js"
+  },
+  {
+    "revision": "c8216ff15ec4e5187d17",
+    "url": "/static/js/runtime-main.829fe273.js"
+  }
+]);
