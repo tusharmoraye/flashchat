080edb6ad266aec863002ea8f02ea44ff8651faf
Tushar Moraye <tusharmoraye@gmail.com>
2020-09-08 12:04:48 +0530

    Add hostname for server to bind to

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -2,6 +2,7 @@ import app from './server';
 
 // Start the server
 const port = Number(process.env.PORT || 3000);
-app.listen(port, () => {
+const host = '0.0.0.0';
+app.listen(port, host, () => {
     console.log('Express server started on port: ' + port);
 });
